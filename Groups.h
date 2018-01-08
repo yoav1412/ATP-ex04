5cb9f744ab5faec850417886c19ad4c5d2f8ee2e
yoav1412 <Yoav.BenEphraim@forter.com>
2018-01-08 20:24:47 +0200

    added .get() function

--- Groups.h
+++ Groups.h
@@ -5,11 +5,10 @@
 #ifndef ATP_EX04_GROUPS_H
 #define ATP_EX04_GROUPS_H
 
-//#define NO_CONSTRAINT std::numeric_limits<int>::min()
-#define NO_CONSTRAINT "N" //todo: reinstate std::numeric_limits<int>::min()
+#define NO_CONSTRAINT std::numeric_limits<int>::min()
 
 #include <map>
-#include <set>
+#include <list>
 #include "PuzzlePiece.h"
 #include "BooleanMasks.h"
 #include <sstream>
@@ -20,41 +19,54 @@ enum Rotate {NO = 0, DEG90 = 90, DEG180 = 180, DEG270 = 270};
 
 template<typename _puzzlePiece>
 class Groups{
-    typedef std::pair<_puzzlePiece*, Rotate> PieceAndRotation;
-    typedef std::string KEY;
+private:
+    typedef std::string key_t;
 
-    std::map<KEY, std::set<PieceAndRotation>> _table;
+    std::map<key_t, std::vector<_puzzlePiece*>> _table;
     Groups();
-
-    KEY pieceToKey(_puzzlePiece *p);
 
 
     /*
      * insert pointer to piece p with all fitting keys in table.
     */
     void insertPiece(_puzzlePiece *p){
-        KEY key;
+        key_t key;
         auto booleanMasks =  BooleanMasks::getBooleanMasks(2*(p->getDim()));
         for (auto booleanMask : booleanMasks){
             std::stringstream strm;
-            for (int i=0; i < 2*(p->getDim()); i++ ){
+            for (int i=0; i < 2*(p->getDim()); i++ ){ //TODO: when we can colon-iterate over piece, would be nicer to use it here.
                 int edge = p->getEdges()[i];
                 if (booleanMask[i]) { strm << edge; }
                 else { strm << NO_CONSTRAINT; }
             }
             key = strm.str();
             if (_table.find(key) == _table.end()) { // key doesn't exist in map yet, so create an empty set
-                _table[key] = std::set<PieceAndRotation>();
+                _table[key] = std::vector<_puzzlePiece*>();
             }
-            _table[key].insert({p, Rotate::NO}); //TODO: currently no support for rotations, add later. (can copy from puzzle constaintsTable)
+            _table[key].push_back(p); //TODO: currently no support for rotations, add later. (can copy from puzzle constaintsTable)
         }
+    };
+
+    key_t pieceToKey(_puzzlePiece &p){
+        std::stringstream strm;
+        for (int i=0; i < 2*p.getDim(); i++) {//TODO: when we can colon-iterate over piece, would be nicer to use it here.
+            int edge = p.getEdges()[i];
+            strm << edge;
+        }
+        return strm.str();
     };
 
 
 
 
 public:
-    //std::vector<PuzzlePiece> get(_puzzlePiece p);
+    std::vector<_puzzlePiece*> get(_puzzlePiece p){
+        key_t key = pieceToKey(p);
+        if (_table.find(key) == _table.end() ){
+            return std::vector<_puzzlePiece*>(); // no piece matches constraint -> return empty vector.
+        }
+        return _table[key];
+    };
 
 
 
--- main.cpp
+++ main.cpp
@@ -9,16 +9,21 @@
 int main(){
 
 
-    int edges1[4] = {1,0,1,1};
-    Puzzle2dPiece<1> p1(edges1);
-
-    int edges2[4] = {0,-1,0,1};
-    Puzzle2dPiece<1> p2(edges2);
+    int edges1[4] = {1,0,1,1}; Puzzle2dPiece<1> p1(edges1);
+    int edges2[4] = {0,-1,0,1}; Puzzle2dPiece<1> p2(edges2);
+    int edges3[4] = {1,1,1,1}; Puzzle2dPiece<1> p3(edges3);
+    int edges4[4] = {1,0,0,0}; Puzzle2dPiece<1> p4(edges4);
 
     std::vector< Puzzle2dPiece<1> > vec;
-    vec.push_back(p1); vec.push_back(p2);
+    vec.push_back(p1); vec.push_back(p2);vec.push_back(p3); vec.push_back(p4);
 
     Groups< Puzzle2dPiece<1> > grps(vec.begin(), vec.end());
+
+    int filter[4] = {NO_CONSTRAINT,NO_CONSTRAINT,NO_CONSTRAINT,1};
+    Puzzle2dPiece<1> filter_piece(filter);
+
+    auto filtered = grps.get(filter_piece);
+
     std::cout <<"DONE\n";
     return 0;
 
